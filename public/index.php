17ccafc8473ba71dac6855c2855121b1d6a2b666
hojinlee <infohojin@gmail.com>
2019-09-04 16:53:38 +0900

    layout

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+$layout = file_get_contents("../theme/index.html");
+
+$filename = $_SERVER['REQUEST_URI'].".htm";
+
+if(file_exists("../".$filename)){
+    $body = file_get_contents("../".$filename);
+    $layout = str_replace("{{main}}",$body, $layout);
+} else {
+    $body = file_get_contents("../home.htm");
+    $layout = str_replace("{{main}}",$body, $layout);
+}
+
+
+echo $layout;
+
+/*
+echo "내가 처음이야";
+
+
+*/
